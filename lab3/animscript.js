3e03edb86cf6f55b484bd58aa981f6a071841492
counter185 <33550839+counter185@users.noreply.github.com>
2023-10-19 14:21:58 +0200

    ta

---
+++ lab3/animscript.js
@@ -0,0 +1,35 @@
+
+function load(){
+    $("#animtest1").on("click", function() {
+        $(this).animate({
+            width: "500px",
+            opacity: 0.4,
+            fontSize: "3em",
+            borderWidth: "10px"
+        }, 1500);
+    });
+
+    $("#animtest2").on({
+        "mouseover": function() {
+            $(this).animate({
+                width: 300
+            }, 800);
+        },
+        "mouseout": function() {
+            $(this).animate({
+                width: 200
+            }, 800);
+        }
+    });
+
+    $("#animtest3").on("click", function(){
+        if (!$(this).is(":animated")) {
+            $(this).animate({
+                width: "+=" + 50,
+                height: "+=" + 10,
+                opacity: "-=" + 0.1,
+                duration: 3000
+            });
+        }
+    });
+}
