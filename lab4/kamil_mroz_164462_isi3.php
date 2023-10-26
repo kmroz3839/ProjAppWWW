ee120dbc272905d1c5977c6af7350fca8a0dddff
counter185 <33550839+counter185@users.noreply.github.com>
2023-10-26 16:00:18 +0200

    lab4 (2/2

--- lab4/kamil_mroz_164462_isi3.php
+++ lab4/kamil_mroz_164462_isi3.php
@@ -1,7 +1,19 @@
-<?
+<?php
   $nr_indeksu = '164462';
   $nrGrupy = 'ISI3';
 
-  echo 'Kamil Mróz'.$nr_indeksu.'grupa '.$nrGrupy.' <br/>';
-  echo 'Zastosowanie metody include() <br/>';
+  echo 'Kamil Mróz '.$nr_indeksu.' grupa '.$nrGrupy.'<br>';
+  echo 'Zastosowanie metody include() <br>';
+
+  if (array_key_exists('name', $_GET)){
+    echo $_GET["name"];
+  }
+  if (array_key_exists('a', $_GET)){
+    for ($i = 0; $i < intval($_GET['a']); $i++){
+      echo $i."<br>";
+    }
+  }
+  
+
+  include 'lib.php';
 ?>
