87e8c5eeb3d2e07afbd29a6d872ea77d8effa304
counter185 <33550839+counter185@users.noreply.github.com>
2023-11-09 16:08:49 +0100

    1.4

---
+++ lab5/html/mainpage.php
@@ -0,0 +1,14 @@
+
+<img class="frontImage" src="img/obrazc1.jpg">
+
+<div id="mainPadded">
+    <div id="mainText">
+    <h1>Strona główna</h1>
+    Na tej stronie znajdziesz wszystko o <span style="font-size:large;"><b>komputerach</b></span> <u>starych i nowych</u>.
+    </div>
+
+    <br>
+    <div id="contactInf">
+        <a href="html/kontakt.html">Kontakt</a>
+    </div>
+</div>
