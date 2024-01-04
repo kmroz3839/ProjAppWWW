9675a623c94d8340d7bb00174698f6f16e3735c2
counter185 <33550839+counter185@users.noreply.github.com>
2024-01-04 17:19:28 +0100

    *

--- lab11/showproduct.php
+++ lab11/showproduct.php
@@ -15,6 +15,13 @@
             <h3>Opis:</h3>
             <br>
             <p>'.$r['opis'].'</p>
+            <br><br>
+            <h2>Cena: '.($r['cena_netto']+$r['podatek_vat']).' zł</h2>
+            <h4>Szczegóły:</h4>
+            Data utworzenia: '.$r['data_utworzenia'].'<br>
+            Data modyfikacji: '.$r['data_modyfikacji'].'<br>
+            Data wygaśnięcia: '.$r['data_wygasniecia'].'<br>
+
 
         ';
     }
